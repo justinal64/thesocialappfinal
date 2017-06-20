91fbfe5f410e2cf7d815edb70023e8d9331336d9
justin leggett <justin@codingrookie.com>
2017-06-20 11:48:05 -0500

    I can post new notes to the db

--- app/screens/Notes.js
+++ app/screens/Notes.js
@@ -20,8 +20,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: [],
-      post: "",
+      userNotes: [],
+      note: "",
       active: true
     };
   }
@@ -32,9 +32,11 @@ export default class App extends Component {
   componentWillReceiveProps() {}
 
   fetchAllData = () => {
-    axios.get(`http://localhost:5000/api/request/getall`).then(res => {
-      this.setState({ userData: res.data });
-    });
+    axios
+      .get(`http://localhost:5000/api/notes/${Global.USERNAME}`)
+      .then(res => {
+        this.setState({ userNotes: res.data });
+      });
   };
 
   clearText = () => {
@@ -42,23 +44,19 @@ export default class App extends Component {
   };
 
   submitToDb = () => {
-    console.log(this.state.post);
-    var newArray = this.state.userData;
-    var newPost = {
-      posts: this.state.post,
+    console.log(this.state.note);
+    var newArray = this.state.userNotes;
+    var newNote = {
+      note: this.state.note,
       dbid: Math.random(),
-      username: Global.USERNAME,
-      likes: 0
+      username: Global.USERNAME
     };
 
-    newArray.unshift(newPost);
-    this.setState({ userDate: newArray });
-    // var newArray = this.state.userData;
-    // this.state.userData.push(newPost);
-    // this.setState({ userData: newArray });
-    axios.post("http://localhost:5000/api/request", {
+    newArray.unshift(newNote);
+    this.setState({ userNotes: newArray });
+    axios.post("http://localhost:5000/api/notes", {
       Username: Global.USERNAME,
-      Posts: this.state.post
+      note: this.state.note
     });
     this.clearText();
   };
@@ -95,24 +93,8 @@ export default class App extends Component {
                 }}
                 ref={component => (this._textInput = component)}
                 placeholder="Add A Note..."
-                onChangeText={post => this.setState({ post })}
+                onChangeText={note => this.setState({ note })}
               />
-              {/*style={{
-              paddingTop: 4,
-              paddingBottom: 4,
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center"
-            }}*/}
-              {/*<View
-                style={{
-                  paddingTop: 4,
-                  paddingBottom: 4,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "center"
-                }}
-              >*/}
               <Button
                 full
                 borderedtextStyle={{ textAlign: "center" }}
@@ -123,121 +105,17 @@ export default class App extends Component {
               {/*</View>*/}
             </View>
           </Card>
-          <Card>
-            <CardItem>
-              <Text>
-                Your text here
-              </Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-              <Text>
-                Your text here
-              </Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-              <Text>
-                Your text here
-              </Text>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-              <Text>
-                Your text here
-              </Text>
-            </CardItem>
-          </Card>
+          {this.state.userNotes.map((note, key) =>
+            <Card key={note.dbid}>
+              <CardItem>
+                <Text>
+                  {note.note}
+                </Text>
+              </CardItem>
+            </Card>
+          )}
         </Content>
       </Container>
     );
   }
 }
-
-// <Container>
-//     <Content>
-//         <Card>
-//             <CardItem>
-//               <Body>
-//                 <Text>
-//                     //Your text here
-//                 </Text>
-//               </Body>
-//             </CardItem>
-//         </Card>
-//     </Content>
-// </Container>
-
-// <Container>
-//   <Header>
-//     <Body>
-//       <Title>Community Activity</Title>
-//     </Body>
-//   </Header>
-//   <Content>
-//     <Card>
-//       <View>
-//         <Input
-//           style={{
-//             flex: 1,
-//             height: 40,
-//             borderColor: "gray",
-//             borderWidth: 1
-//           }}
-//           ref={component => (this._textInput = component)}
-//           placeholder="New Post....."
-//           onChangeText={post => this.setState({ post })}
-//         />
-//         {/*style={{
-//         paddingTop: 4,
-//         paddingBottom: 4,
-//         flexDirection: "row",
-//         alignItems: "center",
-//         justifyContent: "center"
-//       }}*/}
-//         {/*<View
-//           style={{
-//             paddingTop: 4,
-//             paddingBottom: 4,
-//             flexDirection: "row",
-//             alignItems: "center",
-//             justifyContent: "center"
-//           }}
-//         >*/}
-//         <Button
-//           full
-//           borderedtextStyle={{ textAlign: "center" }}
-//           onPress={() => this.submitToDb()}
-//         >
-//           <Text>Submit</Text>
-//         </Button>
-//         {/*</View>*/}
-//       </View>
-//     </Card>
-//     <Body />
-//     <Card>
-//       {this.state.userData.map((post, key) =>
-//         <ListItem avatar key={post.dbid}>
-//           <Left>
-//             <Thumbnail source={require("../img/zoey.jpg")} />
-//           </Left>
-//           <Body>
-//             <Text>{post.username}</Text>
-//             <Text note>
-//               {post.posts}
-//             </Text>
-//           </Body>
-//           <Right>
-//             <Button transparent onPress={() => this.plusOne(post)}>
-//               <Icon active name="thumbs-up" />
-//               <Text>{post.likes} Likes</Text>
-//             </Button>
-//           </Right>
-//         </ListItem>
-//       )}
-//     </Card>
-//   </Content>
-// </Container>
